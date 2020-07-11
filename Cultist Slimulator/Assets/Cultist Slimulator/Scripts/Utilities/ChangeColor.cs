a73e1e281881364e09e50477254c48b97d6dfdb1
D.G. Colasito <dgcolasito11@gmail.com>
2020-07-11 16:43:28 +0800

    Timer and events

---
+++ Cultist Slimulator/Assets/Cultist Slimulator/Scripts/Utilities/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace cc.tools
+{
+    public class ChangeColor : MonoBehaviour
+    {
+        Image img;
+        public Color tint = Color.white;
+        void Start()
+        {
+            img = GetComponent<Image>();
+        }
+
+        public void colorChange()
+        {
+            img.color = tint;
+        }
+    }
+
+}
---
+++ Cultist Slimulator/Assets/Cultist Slimulator/Scripts/Utilities/DelayedEvent.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace cc.tools
+{
+    public class DelayedEvent : MonoBehaviour
+    {
+        public List<UnityEvent> Eventlist = new List<UnityEvent>();
+      
+        public void CallEvent()
+        {
+            if (Eventlist.Count <= 0) return;
+
+            for (int i =0; i < Eventlist.Count;i++)
+            {
+              if(Eventlist[i]!=null)
+              {
+                Eventlist[i].Invoke();
+              }
+            }           
+              
+        }
+
+        public void CallDelayedEvent(float delay)
+        {
+            StartCoroutine(Delay(delay));
+
+        }
+
+        IEnumerator Delay(float delay)
+        {
+          
+          for (int i = 0; i < Eventlist.Count; i++)
+          {
+            if (Eventlist[i] != null)
+            {
+                Eventlist[i].Invoke();
+            }
+            yield return new WaitForSeconds(delay);
+
+          }
+            StopAllCoroutines();
+           
+        }
+
+    }
+
+}
