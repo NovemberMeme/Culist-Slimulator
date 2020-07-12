c6b0f9902e1e3fff3809f007581b54097ca967c5
D.G. Colasito <dgcolasito11@gmail.com>
2020-07-12 23:19:54 +0800

    icons

---
+++ Cultist Slimulator/Assets/Cultist Slimulator/Scripts/Utilities/MainMenuControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace cc.tools
+{
+    public enum command
+    {
+        START,
+        ABOUT,
+        EXIT,
+        NONE
+    }
+    public class MainMenuControl : MonoBehaviour
+    {
+        [SerializeField] int sceneIndex = 1;
+        [HideInInspector]
+        public Draggable Selection;
+        public command type = command.NONE;
+         
+        public void LoadLevel()
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        public void Close()
+        {
+            Application.Quit();
+        }
+
+        
+
+
+
+    }
+
+}
---
+++ Cultist Slimulator/Assets/Cultist Slimulator/Scripts/Utilities/MainMenuMicrowave.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+namespace cc.tools
+{
+    public class MainMenuMicrowave : MonoBehaviour, IDropHandler
+    {
+
+        [SerializeField] MainMenuControl mmc;
+        public UnityEvent OnStart = new UnityEvent();
+        public UnityEvent OnAbout = new UnityEvent();
+        public UnityEvent OnExit = new UnityEvent();
+     
+      
+        public void OnDrop(PointerEventData eventData)
+        {
+            Debug.Log("frap");
+            if (eventData.pointerDrag != null)
+            {
+               switch (mmc.type)
+                {
+                    case command.START:
+                        OnStart?.Invoke();
+                        break;
+                    case command.ABOUT:
+                        OnAbout?.Invoke();
+                        break;
+                    case command.EXIT:
+                        OnExit?.Invoke();
+                        break;
+
+                    default:
+                        return;
+                        
+
+                }
+            }
+
+        }
+
+    }
+
+
+}
+
